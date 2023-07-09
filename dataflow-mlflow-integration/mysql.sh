79f7f13cd1ea8f2aebca175c29de8aebeaed2444
Nilay Patel <0aayyy2107@gmail.com>
2023-07-09 19:17:14 +0530

    Update mysql.sh
    Removed Private URIs

--- dataflow-mlflow-integration/mysql.sh
+++ dataflow-mlflow-integration/mysql.sh
@@ -10,15 +10,6 @@ package_exists() {
   fi
 }
 
-# Function to check if a string is empty or blank
-is_blank() {
-  if [[ -z "$1" ]]; then
-    return 0  # String is blank
-  else
-    return 1  # String is not blank
-  fi
-}
-
 if ! package_exists mysql-shell; then
   sudo yum install -y mysql-shell
 fi
@@ -27,20 +18,13 @@ fi
 echo "Enter the username of the Database:"
 read username
 
-# If username is blank, use default credentials
-if is_blank "$username"; then
-  username="admin"
-  password="Mlflow@2023"
-  hostname="mysqldb.sub01292340360.datasciencevcn.oraclevcn.com"
-  dbname="mlflow"
-else
-  echo "Enter the Hostname:"
-  read hostname
-  echo "Enter the Password:"
-  read -s password
-  echo "Enter the Database name:"
-  read dbname
-fi
+
+echo "Enter the Hostname:"
+read hostname
+echo "Enter the Password:"
+read -s password
+echo "Enter the Database name:"
+read dbname
 
 # Create Database
 mysqlsh "$username@$hostname" --password="$password" --sql -e "CREATE DATABASE $dbname;"
